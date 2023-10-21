e799927de7bd92de9e5e4bbce0775759d9d5e1ff
glitton <glcodeworks@gmail.com>
2023-10-21 15:33:05 -0500

    more blogs added

---
+++ components/Video.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+
+const videoResponsive = {
+  overflow: 'hidden',
+  paddingBottom: '56.25%',
+  position: 'relative',
+  height: 0,
+}
+
+const Video = ({ embedId }) => (
+  <div style={videoResponsive}>
+    <iframe
+      style={{ left: 0, top: 0, height: '100%', width: '100%', position: 'absolute' }}
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+)
+
+Video.propTypes = {
+  embedId: PropTypes.string.isRequired,
+}
+
+export default Video
