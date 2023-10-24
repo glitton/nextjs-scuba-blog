7478af79c8f73519a30db3dd9d23e880f2bfb472
glitton <glcodeworks@gmail.com>
2023-10-23 23:08:59 -0500

    used youtube.com

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is youtube-nocookie.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is youtube.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app youtube.com
 `
 
 const securityHeaders = [
@@ -53,15 +53,15 @@ const securityHeaders = [
     value: 'camera=(), microphone=(), geolocation=()',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/frame-ancestors
-  {
-    key: 'Frame-Ancestors',
-    value: 'youtube-nocookie.com',
-  },
+  // {
+  //   key: 'Frame-Ancestors',
+  //   value: 'youtube-nocookie.com',
+  // },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/frame-src
-  {
-    key: 'frame-src',
-    value: 'https://www.youtube-nocookie.com/embed/*',
-  },
+  // {
+  //   key: 'frame-src',
+  //   value: 'https://www.youtube-nocookie.com/embed/*',
+  // },
 ]
 
 /**
