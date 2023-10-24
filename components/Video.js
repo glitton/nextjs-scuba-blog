ba80afde2bcb22c7d904ca8ccc2ded1fd9150664
glitton <glcodeworks@gmail.com>
2023-10-23 20:59:06 -0500

    added nocookie to youtube link

--- components/Video.js
+++ components/Video.js
@@ -13,7 +13,7 @@ const Video = ({ embedId }) => (
       style={{ left: 0, top: 0, height: '100%', width: '100%', position: 'absolute' }}
       width="853"
       height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      src={`https://www.youtube-nocookie.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
