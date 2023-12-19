5fa0224dac94f04993da541934383ac05660fa3a
glitton <glcodeworks@gmail.com>
2023-12-19 01:30:29 -0800

    fixed blog day 1 typos

--- data/headerNavLinks.ts
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: 'https://glcodeworks.com', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
