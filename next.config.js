c99dde48c7e58af1ba648353f4aed7af35d69c09
glitton <glcodeworks@gmail.com>
2023-12-18 16:16:23 -0800

    still wip

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://youtube-nocookie.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src *.s3.amazonaws.com *.mux.com *.youtube*.com;
+  media-src *.s3.amazonaws.com, mux.com;
   connect-src *;
   font-src 'self';
   frame-src giscus.app https://youtube-nocookie.com
