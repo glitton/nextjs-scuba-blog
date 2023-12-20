a56530441506b9447f62fc3dc604ea6e50b19e58
Generosa Litton <glcodeworks@gmail.com>
2023-12-20 13:44:47 -0500

    updated logo.svg with glcodeworks logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,9 +6,9 @@ const siteMetadata = {
   description: 'Sharing the wonders of the ocean',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://blog.glcodeworks.com/blog',
   siteRepo: 'https://github.com/glitton/nextjs-scuba-blog',
-  siteLogo: 'logo.svg',
+  siteLogo: 'glcodeworks-logo.svg',
   socialBanner: '/static/images/twitter-card.png',
   email: 'generosa@glcodeworks.com',
   github: 'https://github.com/glitton',
