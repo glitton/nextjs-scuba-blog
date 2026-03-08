b727428b7ad8881a7f799f50817d005b03f79a92
Generosa Litton <glcodeworks@gmail.com>
2026-03-08 06:11:16 -0500

    Changed blog title and added coming soon

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,10 +12,13 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest Blogs
+            Blogs: 2022 to 2025
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
+          </p>
+          <p className="text-sm leading-7 text-gray-500 dark:text-gray-400">
+            Click here for the latest blogs (coming soon)
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
